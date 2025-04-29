1618dc1e258e8b579cc8bca1496d6d268d8c7ac1
Ganesh17032009ramesh <ganeshramesh173@gmail.com>
2025-04-29 19:24:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = os.environ.get("20902603")
-API_HASH = os.environ.get("79e5caa103a9e9fb0183390b4800845d")
-BOT_TOKEN = os.environ.get("8025198727:AAF7sQa6srVJwKOxhRyUtgk_rbKG2p26ZC8")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
